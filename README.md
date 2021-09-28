c18a88f6726fd436ce0013cbd86c26b2db1a2a27
Lautaro Blanco <blanco.lautaro@outlook.com>
2021-09-28 18:49:35 -0300

    Export entrega 3

--- README.md
+++
@@ -1,2 +0,0 @@
-<h1>Introduccion al Desarrollo de Videojuegos</h1>
-<h3>Entregas - Lautaro Blanco</h3>
--- entrega-2-blanco-lautaro/Main.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-func _ready():
-	$Player.set_bullet_container(self)
-	$Turret.set_values($Player, self)
-	$Turret2.set_values($Player, self)
